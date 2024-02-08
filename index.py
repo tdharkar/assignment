eda17a782b8dae8dc0af57d5e0bd7c744f8b827a
tdharkar <159418123+tdharkar@users.noreply.github.com>
2024-02-08 18:47:57 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("5050"), debug=True)
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
